9d70a7721fd0637a35ca9ed2856ffd4d5fadfaac
Ramona Stefanova <ramona.stefanova@myy.haaga-helia.fi>
2022-10-19 20:35:27 +0300

    First commit

---
+++ Bookstore/src/main/java/fi/haagahelia/Bookstore/domain/Book.java
@@ -0,0 +1,76 @@
+package fi.haagahelia.Bookstore.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String title, author, publicationYear, isbn;
+	private double price;
+
+	public Book() {
+
+	}
+
+	public Book(String title, String author, String publicationYear, String isbn, double price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.publicationYear = publicationYear;
+		this.isbn = isbn;
+		this.price = price;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPublicationYear() {
+		return publicationYear;
+	}
+
+	public void setPublicationYear(String publicationYear) {
+		this.publicationYear = publicationYear;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
---
+++ Bookstore/src/main/java/fi/haagahelia/Bookstore/web/BookController.java
@@ -0,0 +1,15 @@
+package fi.haagahelia.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+	@RequestMapping("/index")
+	public String index() {
+
+		return "index";
+	}
+
+}
