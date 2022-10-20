9193f92bfdab5a18326414699e278ed69b43aba5
Ramona Stefanova <ramona.stefanova@myy.haaga-helia.fi>
2022-10-20 14:40:04 +0300

    Add Crud functionalities

--- Bookstore/src/main/java/fi/haagahelia/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/fi/haagahelia/Bookstore/web/BookController.java
@@ -3,8 +3,11 @@ package fi.haagahelia.Bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import fi.haagahelia.Bookstore.domain.Book;
 import fi.haagahelia.Bookstore.domain.BookRepository;
 
 @Controller
@@ -18,11 +21,29 @@ public class BookController {
 
 		return "index";
 	}
-	
+
 	@RequestMapping("/booklist")
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
-		
+
+	}
+
+	@RequestMapping("/add")
+	public String addStudent(Model model) {
+		model.addAttribute("student", new Book());
+		return "addstudent";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
 	}
 }
